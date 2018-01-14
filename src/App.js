b110e02811f0d753b7b110d45da58c676cb61f90
shiying <wonder_sy0618@foxmail.com>
2018-01-14 13:00:35 +0800

    去掉无用的文件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Button from 'antd/lib/button';
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
