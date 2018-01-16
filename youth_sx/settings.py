4714b554febfedd611e812d19b1772afd8a22887
shiying <wonder_sy0618@foxmail.com>
2018-01-16 20:57:23 +0800

    增加静态资源处理方案

--- youth_sx/settings.py
+++ youth_sx/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/docs/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "docs"),
+    '/var/www/static/',
+]
 
 
 CORS_ALLOW_CREDENTIALS = True
