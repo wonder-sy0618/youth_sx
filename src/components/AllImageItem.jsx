54feba333514c11ac4e3e8e887ea9b729a47f28f
shiying <wonder_sy0618@foxmail.com>
2018-01-23 20:05:05 +0800

    调整图片块的样式

--- src/components/AllImageItem.jsx
+++ src/components/AllImageItem.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 
 import "./AllImageItem.css"
+import iconLocal from "../res/ic_local.png"
 
 export default (props) => {
   return (
     <div className={props.item.imghdw < 1 ? "imgItem item_horizontal_1" : "imgItem item_vertical_1" } >
       <img src={props.item.imgid} />
-      <div className="name" >我是陕西好青年<span>{props.item.iname}</span></div>
-      <div className="local" >我在{props.item.iwhere.replace("陕西省,", "").replace(",", "")}</div>
-      <div className="declaration_title" >我的青春宣言是：</div>
-      <div className="declaration_content" >{props.item.itext}</div>
+      <div className="itemTextBox" >
+        <div className="itemTextContent" >
+          我是陕西好青年，{props.item.itext}
+        </div>
+        <div className="itemTextName" >
+          -- {props.item.iname}
+        </div>
+        <div className="itemTextLocal" >
+          <img src={iconLocal} />
+          我在{props.item.iwhere.replace("陕西省,", "").replace(",", "")}
+        </div>
+      </div>
     </div>
   );
 }
