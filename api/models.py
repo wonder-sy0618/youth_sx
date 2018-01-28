07d17c5fd810060c5c061098a4363fc3f93ede6d
shiying <wonder_sy0618@foxmail.com>
2018-01-28 10:22:31 +0800

    增加数据字段容量

--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     uid = models.CharField(max_length=50)
     addtime = models.CharField(max_length=14)
     status_remove = models.IntegerField()
-    imgid = models.CharField(max_length=20)
+    imgid = models.CharField(max_length=200)
     imghdw = models.FloatField(default=1)
     iname = models.CharField(max_length=20, default="陕西青年")
     iam = models.CharField(max_length=20, default="陕西青年")
