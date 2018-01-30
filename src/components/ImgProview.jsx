a33491ce1798e781edb44c878874b6fb4a01108c
shiying <wonder_sy0618@foxmail.com>
2018-01-30 10:03:14 +0800

    修改预览图片效果

--- src/components/ImgProview.jsx
+++ src/components/ImgProview.jsx
@@ -7,6 +7,18 @@ import qrcode from "qrcode"
 import Result from "antd-mobile/lib/result"
 import Icon from "antd-mobile/lib/icon"
 
+import Konva from "konva"
+import iconLocal from "../res/ic_local.png"
+
+const conf = {
+  width : 1242,
+  heightAdd : 400,
+  bgColor : "white",
+  bgImageBorder : 30,
+  iTextMarginBottom : 50,
+  qrcodeSize : 360
+}
+
 export default class ImgProview extends Component {
 
   constructor() {
@@ -16,273 +28,182 @@ export default class ImgProview extends Component {
       }
   }
 
-  qrcode(canvas, item) {
-    imgtreat.textDraw(canvas, "邀请您关注",
-      102, canvas.height - 98, {
-        fillStyle : "white",
-        font : '50px FZShuTi,STXihei,Arial',
-        lineSpacing : 1.2,
-        middlePos : false
-      })
-    imgtreat.textDraw(canvas, "邀请您关注",
-      100, canvas.height - 100, {
-        fillStyle : "black",
-        font : '50px FZShuTi,STXihei,Arial',
-        lineSpacing : 1.2,
-        middlePos : false
-      })
-    {
-      let ctx = canvas.getContext("2d");
-      ctx.lineJoin = "round";
-      ctx.lineWidth = 4;
-      ctx.strokeStyle="white";
-      ctx.strokeRect(375, canvas.height - 90, 260, 64);
-    }
-    imgtreat.textDraw(canvas, "陕西好青年",
-      380, canvas.height - 100, {
-        fillStyle : "rgba(195, 177, 0, 0.89)",
-        font : '50px FZShuTi,STXihei,Arial',
-        lineSpacing : 1.2,
-        middlePos : false
-      })
-    imgtreat.textDraw(canvas, "➤",
-      660, canvas.height - 100, {
-        fillStyle : "white",
-        font : '50px FZShuTi,STXihei,Arial',
-        lineSpacing : 1.2,
-        middlePos : false
-      })
-    let url = window.location.href.substring(0, window.location.href.indexOf("#") >= 0
-                                        ? window.location.href.indexOf("#")
-                                        : window.location.href.length)
-    return qrcode.toDataURL(
-        url,
-        {
-          margin : 2,
-          width : 128,
-          height : 128
+  loadData() {
+    let comp = this;
+    return new Promise((resolve, reject) => {
+      jquery.ajax({
+        url : config.apiBase + "list?id=" + comp.props.match.params.id,
+        dataType : 'json',
+        success : function(json) {
+          let item = json.length > 0 ? json[0] : undefined;
+          comp.setState({
+            item : item
+          })
+          resolve(item)
         }
-      ).then(url => imgtreat.imageOpen(
-        url
-      )).then(img => {
-        canvas.getContext('2d').drawImage(img, 730, canvas.height - img.height - 10, img.width, img.height)
+      });
+    })
+  }
+
+  loadImage(src) {
+    return new Promise((resolve, reject) => {
+      let imageObj = new Image();
+      imageObj.onload = function() {
+        resolve(imageObj)
+      }
+      imageObj.setAttribute("crossOrigin",'Anonymous');
+      imageObj.src = src;
+    });
+  }
+
+  buildText(text, maxWidth, fontSize, fontFamily) {
+    let lines = [""]
+    for (var i=0; i<text.length; i++) {
+      let str = lines[lines.length-1] + text[i];
+      if (new Konva.Text({
+        x: 0,
+        y: 0,
+        text: str,
+        fontSize: fontSize,
+        fontFamily: fontFamily
+      }).width() <= maxWidth) {
+        lines[lines.length-1] = lines[lines.length-1] + text[i]
+
+      } else {
+        lines[lines.length-1] = lines[lines.length-1]
+        lines.push("")
+      }
+    }
+    return lines.join("\n");
+  }
+
+  drawElement(layer, x, y, w, h) {
+    let comp = this;
+    return new Promise(resolve => {
+        resolve();
+    }).then(() => {
+      // 宣言文字
+      let itext = "我是陕西好青年" + comp.state.item.iname + ", " + comp.state.item.itext;
+      itext = itext + itext
+      let itextFontFamily = "LiSu";
+      let itextFontSize = 60;
+      let kt = new Konva.Text({
+        x: x,
+        y: y,
+        text: comp.buildText(itext, w - conf.qrcodeSize, itextFontSize, itextFontFamily),
+        fontSize: itextFontSize,
+        fontFamily: itextFontFamily,
+        fill: 'black'
+      });
+      layer.add(kt);
+      y = y;
+      h = h - kt.height() + conf.iTextMarginBottom;
+
+    }).then(() => {
+      // 转换二维码
+      return qrcode.toDataURL(
+        window.location.href.substring(0, window.location.href.indexOf("#") >= 0
+                                            ? window.location.href.indexOf("#")
+                                            : window.location.href.length),
+          {
+            margin : 2,
+            width : 128,
+            height : 128
+          }
+        )
+
+    }).then(url => {
+      // 加载二维码图片
+      return this.loadImage(url)
+
+    }).then(imageQrcode => {
+      // 绘制图片
+      let ki = new Konva.Image({
+        x: x + w - conf.qrcodeSize,
+        y: y,
+        width : conf.qrcodeSize,
+        height : conf.qrcodeSize,
+        image: imageQrcode
+      });
+      layer.add(ki)
+      w = w - conf.qrcodeSize
+
+    }).then(() => {
+      // 加载位置图标
+      return comp.loadImage(iconLocal);
+
+    }).then(img => {
+      // 输出位置图标
+      layer.add(new Konva.Image({
+        x: x,
+        y: y + conf.qrcodeSize - 70,
+        width : 50,
+        height : 50,
+        image: img
+      }))
+      // 输出位置
+      layer.add(new Konva.Text({
+        x: x + 50,
+        y: y + conf.qrcodeSize - 50,
+        text: "我是" + comp.state.item.iwhere.replace("陕西省,","").replace(",","") + "第" + comp.state.item.iwhereid + "位陕西好青年代言人",
+        fontSize: 36,
+        fontFamily: "微软雅黑",
+        fill: 'black'
+      }))
+
+    })
+
+  }
+
+  draw() {
+    let comp = this;
+    return this.loadImage(this.state.item.imgid).then(imageBg => {
+      var stage = new Konva.Stage({
+        container: 'container',
+        width: conf.width,
+        height: ((conf.width - conf.bgImageBorder * 2) * comp.state.item.imghdw + conf.heightAdd + conf.bgImageBorder * 2)
+      });
+      var layer = new Konva.Layer();
+      // 背景色
+      layer.add(new Konva.Rect({
+        x: 0,
+        y: 0,
+        width: stage.width(),
+        height: stage.height(),
+        fill: conf.bgColor
+      }));
+      // 主图
+      layer.add(new Konva.Image({
+        x: conf.bgImageBorder,
+        y: conf.bgImageBorder,
+        image: imageBg,
+        width: (stage.width() - conf.bgImageBorder * 2),
+        height: (stage.width() - conf.bgImageBorder * 2) * comp.state.item.imghdw
+      }));
+      // 绘制其它元素
+      this.drawElement(
+        layer,
+        conf.bgImageBorder,       // 其它元素空间x
+        conf.bgImageBorder * 2 + (stage.width() - conf.bgImageBorder * 2) * comp.state.item.imghdw,   // 其它元素空间y
+        stage.width() - conf.bgImageBorder * 2,   // 元素空间宽度
+        conf.heightAdd        // 元素空间高度
+      ).then(() => {
+        // 输出
+        stage.add(layer);
+        // 转换
+        var imageDataUrl = jquery("#container canvas")[0].toDataURL();
+        comp.setState({
+          proview : imageDataUrl
+        })
       })
-  }
-
-  // 横图
-  imageHorizontal(canvas, item) {
-    imgtreat.textDraw(canvas, "我是" + item.iwhere.replace("陕西省,","").replace(",","") + "第" + item.iwhereid + "位陕西好青年代言人",
-        canvas.width - 40, 10, {
-          fillStyle : "white",
-          font : '20px Arial',
-          lineSpacing : 1.2,
-          middlePos : false,
-          isVertical : true
-        })
-    //
-    imgtreat.textDraw(canvas, "我是陕西好青年",
-        80, 100, {
-          fillStyle : "black",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, "我是陕西好青年",
-        75, 95, {
-          fillStyle : "white",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, item.iname,
-        380, 70, {
-          fillStyle : "blue",
-          font : '60px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, "我在" + item.iwhere.replace("陕西省,","").replace(",",""),
-        285, 175, {
-          fillStyle : "black",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, "我在" + item.iwhere.replace("陕西省,","").replace(",",""),
-        280, 170, {
-          fillStyle : "white",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, "我的青春宣言是",
-        71, 291, {
-          fillStyle : "white",
-          font : '46px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, "我的青春宣言是",
-        70, 290, {
-          fillStyle : "red",
-          font : '46px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, item.itext.replace("，", "，\n").replace(",", "，\n"),
-        171, 371, {
-          fillStyle : "white",
-          font : '46px STKaiti,SimHei,"STXinwei","微软雅黑",sans-serif',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, item.itext.replace("，", "，\n").replace(",", "，\n"),
-        170, 370, {
-          fillStyle : "red",
-          font : '46px STKaiti,SimHei,"STXinwei","微软雅黑",sans-serif',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    return this.qrcode(canvas, item);
-  }
-
-  // 纵图
-  imageVertical(canvas, item) {
-    imgtreat.textDraw(canvas, "我是" + item.iwhere.replace("陕西省,","").replace(",","") + "第" + item.iwhereid + "位陕西好青年代言人",
-        canvas.width - 60, 10, {
-          fillStyle : "white",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          isVertical : true
-        })
-    //
-    imgtreat.textDraw(canvas, "我是陕西好青年",
-        80, 130, {
-          fillStyle : "black",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, "我是陕西好青年",
-        75, 125, {
-          fillStyle : "white",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, item.iname,
-        380, 100, {
-          fillStyle : "blue",
-          font : '60px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, "我在" + item.iwhere.replace("陕西省,","").replace(",",""),
-        285, 205, {
-          fillStyle : "black",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, "我在" + item.iwhere.replace("陕西省,","").replace(",",""),
-        280, 200, {
-          fillStyle : "white",
-          font : '40px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, "我的青春宣言是",
-        71, 621, {
-          fillStyle : "white",
-          font : '46px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, "我的青春宣言是",
-        70, 620, {
-          fillStyle : "red",
-          font : '46px Arial',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    //
-    imgtreat.textDraw(canvas, item.itext.replace("，", "，\n").replace(",", "，\n"),
-        171, 701, {
-          fillStyle : "white",
-          font : '46px STKaiti,SimHei,"STXinwei","微软雅黑",sans-serif',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    imgtreat.textDraw(canvas, item.itext.replace("，", "，\n").replace(",", "，\n"),
-        170, 700, {
-          fillStyle : "red",
-          font : '46px STKaiti,SimHei,"STXinwei","微软雅黑",sans-serif',
-          lineSpacing : 1.2,
-          middlePos : false
-        })
-    return this.qrcode(canvas, item);
+    })
   }
 
   componentDidMount() {
     let comp = this;
-    jquery.ajax({
-      url : config.apiBase + "list?id=" + comp.props.match.params.id,
-      dataType : 'json',
-      success : function(json) {
-        let item = json.length > 0 ? json[0] : undefined;
-        comp.setState({
-          item : item
-        })
-        imgtreat.imageOpen(
-          item.imgid
-        ).then(img => {
-          // 计算宽高
-          // let width = window.innerWidth * 2;
-          let width = 1000;
-          let height = img.height * width / img.width
-          //
-          imgtreat.create(
-            width, height
-          ).then(canvas => {
-            // 底图
-            canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height)
-            //
-            if (item.imghdw > 1) {
-              // 纵图
-              comp.imageVertical
-                .apply(comp, [canvas, item])
-                .then(() => {
-                  // 输出图片
-                  comp.setState({
-                    proview : canvas.toDataURL("image/png")
-                  })
-                })
-            } else {
-              // 横图
-              comp.imageHorizontal
-                .apply(comp, [canvas, item])
-                .then(() => {
-                  // 输出图片
-                  comp.setState({
-                    proview : canvas.toDataURL("image/png")
-                  })
-                })
-            }
-            // 输出图片
-            comp.setState({
-              proview : canvas.toDataURL("image/png")
-            })
-          })
-        })
-      }
-    });
+    this.loadData().then(() => {
+      comp.draw();
+    })
   }
 
   render() {
@@ -296,6 +217,7 @@ export default class ImgProview extends Component {
           title="上传成功"
           message="请长按下方的图片，保存到手机"
         />
+        <div id="container" style={{"display" : "none"}} ></div>
         <div style={{ padding: 10 }}>
           <img src={this.state.proview}
               style={{
